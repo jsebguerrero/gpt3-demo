3ef8221591940a8f7da6bfa89b7d49d9f11510ef
Juan S Guerrero C <36944059+jsebguerrero@users.noreply.github.com>
2023-02-24 10:27:20 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import openai 
+import streamlit as st
+
+# pip install streamlit-chat  
+from streamlit_chat import message
+
+openai.api_key = st.secrets["api_secret"]
+def generate_response(prompt):
+    completions = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt + '\n\n###\n\n',
+        temperature=0.5,
+        max_tokens=300,
+        top_p=0.3,
+        frequency_penalty=0.5,
+        presence_penalty=0.0
+        )
+    message = completions.choices[0].text
+    return message
+st.title("KONECTA AI & DS - Karin GPT-3")
+
+# Storing the chat
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+# We will get the user's input by calling the get_text function
+def get_text():
+    input_text = st.text_input("You: ","Hola", key="input")
+    return input_text
+
+user_input = get_text()
+
+if user_input:
+    output = generate_response(user_input)
+    # store the output 
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
+
+if st.session_state['generated']:
+    
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
